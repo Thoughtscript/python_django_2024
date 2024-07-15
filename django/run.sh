e2e9e73dea0a537154b9ec0d84b2a5f62604ac91
Adam Gerard <adam.intae.gerard@protonmail.com>
2024-07-15 17:40:34 -0500

    tweak bash

--- django/run.sh
+++ django/run.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Waiting 25 seconds..."
-
-sleep 25
-
-echo "Migrating DB and running server"
-
-echo "Waiting 5-10 seconds..."
-
-# sleep 5 && python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py seed && python3 manage.py runserver 0.0.0.0:8000 &
-
-sleep 5 && python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py seed &
+echo "Waiting 25 seconds..." && sleep 25 && echo "Migrating DB and running server" && python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py seed &
 
 # https://dev.to/pragativerma18/unlocking-performance-a-guide-to-async-support-in-django-2jdj
 # https://www.uvicorn.org/settings/#application
-sleep 10 && uvicorn djangoexample.asgi:application --host 0.0.0.0 --port 8000 --workers 4 & 
+echo "Waiting 25 seconds..." && sleep 25 && echo "Waiting 5-10 seconds..." && sleep 10 && uvicorn djangoexample.asgi:application --host 0.0.0.0 --port 8000 --workers 4 & 
 
 wait
